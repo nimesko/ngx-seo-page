ee8e35d58145b0bf62f3a4df3c31389c2faf5f3f
Rogerio Tristão Junior <rogerio.tju@gmail.com>
2019-05-09 21:38:54 -0300

    docs: adding documentation on service and readme

--- projects/ngx-seo-page/src/lib/page.service.ts
+++ projects/ngx-seo-page/src/lib/page.service.ts
@@ -30,6 +30,10 @@ export class PageService {
     this.metatags = [];
   }
 
+  /**
+   * Refreshes the page using the input parameters.
+   * @param page parameter that defines what will refresh on the page
+   */
   updatePage(page: Page): void {
     this.title.setTitle(page.title);
     this.updateMetatags(page.metatags);
@@ -37,6 +41,11 @@ export class PageService {
     this.updateMicrodata(page.microdata);
   }
 
+  /**
+   * Method that updates metatags, its first action is to remove previously registered metatags to put the new ones.
+   * If no metatags was passed, just remove the previous.
+   * @param metatags metatags that will be applied
+   */
   updateMetatags(metatags: MetaDefinition[] = []): void {
     this.metatags.map(metatag => this.meta.removeTagElement(metatag));
     if (metatags) {
@@ -44,6 +53,10 @@ export class PageService {
     }
   }
 
+  /**
+   * Method that adds or updates the microdata in the DOM or, if it has no parameters, removes it from the DOM.
+   * @param microdata microdata that will be used to apply to a JSON + LD script or, in case of absence of the parameter, its removal
+   */
   updateMicrodata(microdata?: object): void {
     if (microdata) {
       this.microdataElement.text = JSON.stringify({ ...this.microdataBaseObject, ...microdata });
@@ -57,6 +70,11 @@ export class PageService {
     }
   }
 
+  /**
+   * Method that will add in the DOM the element that indicates the canonical link or, if no link is passed,
+   * will remove the element from the DOM, but will keep in memory.
+   * @param link canonical link that will be applied or, if nothing is passed, remove the previously applied link
+   */
   updateCanonicalLink(link?: string): void {
     if (link) {
       this.canonicalLinkElement.href = link;
