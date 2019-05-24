cb525c2e3fbf3d253f050179bbcaa1ceaa903789
Rogerio Tristão Junior <rogerio.tju@gmail.com>
2019-05-24 12:15:35 -0300

    refactor: improving the way that handle schema
    BREAKING CHANGE: replaced 'microdata' to 'schema' on Page model and using the type 'SchemaData' to
    describe it better

--- projects/ngx-seo-page/src/lib/page.service.ts
+++ projects/ngx-seo-page/src/lib/page.service.ts
@@ -2,7 +2,7 @@ import { DOCUMENT } from '@angular/common';
 import { Inject, Injectable } from '@angular/core';
 import { Meta, MetaDefinition, Title } from '@angular/platform-browser';
 
-import { Page } from './page.model';
+import { Page, SchemaData } from './page.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,8 @@ export class PageService {
 
   private metatags: HTMLMetaElement[];
 
-  private readonly microdataBaseObject: object;
-  private readonly microdataElement: HTMLScriptElement;
+  private readonly schemaBaseObject: object;
+  private readonly schemaElement: HTMLScriptElement;
   private readonly canonicalLinkElement: HTMLLinkElement;
 
   constructor(
@@ -20,71 +20,83 @@ export class PageService {
     private title: Title,
     private meta: Meta
   ) {
-    this.microdataElement = this.document.createElement('script');
+    this.schemaElement = this.document.createElement('script');
     this.canonicalLinkElement = this.document.createElement('link');
     this.canonicalLinkElement.setAttribute('rel', 'canonical');
-    this.microdataElement.type = 'application/ld+json';
-    this.microdataBaseObject = {
+    this.schemaElement.type = 'application/ld+json';
+    this.schemaBaseObject = {
       '@context': 'http://schema.org'
     };
     this.metatags = [];
   }
 
-  /**
-   * Refreshes the page using the input parameters.
-   * @param page parameter that defines what will refresh on the page
-   */
   updatePage(page: Page): void {
-    this.title.setTitle(page.title);
+    this.setTitle(page.title);
     this.updateMetatags(page.metatags);
     this.updateCanonicalLink(page.canonical);
-    this.updateMicrodata(page.microdata);
+    this.updateSchema(page.schema);
   }
 
-  /**
-   * Method that updates metatags, its first action is to remove previously registered metatags to put the new ones.
-   * If no metatags was passed, just remove the previous.
-   * @param metatags metatags that will be applied
-   */
-  updateMetatags(metatags: MetaDefinition[] = []): void {
+  setTitle(title?: string): void {
+    if (title) {
+      this.title.setTitle(title);
+    }
+  }
+
+  setMetatags(metatags: MetaDefinition[]): void {
+    this.metatags.push(...metatags.map(metatag => this.meta.addTag(metatag)));
+  }
+
+  removeMetatags(): void {
     this.metatags.map(metatag => this.meta.removeTagElement(metatag));
+  }
+
+  updateMetatags(metatags: MetaDefinition[] = []): void {
+    this.removeMetatags();
     if (metatags) {
-      this.metatags = metatags.map(metatag => this.meta.addTag(metatag));
+      this.setMetatags(metatags);
     }
   }
 
-  /**
-   * Method that adds or updates the microdata in the DOM or, if it has no parameters, removes it from the DOM.
-   * @param microdata microdata that will be used to apply to a JSON + LD script or, in case of absence of the parameter, its removal
-   */
-  updateMicrodata(microdata?: object): void {
-    if (microdata) {
-      this.microdataElement.text = JSON.stringify({ ...this.microdataBaseObject, ...microdata });
-      if (!this.document.contains(this.microdataElement)) {
-        this.document.body.appendChild(this.microdataElement);
-      }
+  setSchema(schema: SchemaData): void {
+    this.schemaElement.text = JSON.stringify({ ...this.schemaBaseObject, ...schema });
+    if (!this.document.contains(this.schemaElement)) {
+      this.document.body.appendChild(this.schemaElement);
+    }
+  }
+
+  removeSchema(): void {
+    if (this.document.contains(this.schemaElement)) {
+      this.schemaElement.remove();
+    }
+  }
+
+  updateSchema(schema?: SchemaData): void {
+    if (schema) {
+      this.setSchema(schema);
     } else {
-      if (this.document.contains(this.microdataElement)) {
-        this.microdataElement.remove();
-      }
+      this.removeSchema();
     }
   }
 
-  /**
-   * Method that will add in the DOM the element that indicates the canonical link or, if no link is passed,
-   * will remove the element from the DOM, but will keep in memory.
-   * @param link canonical link that will be applied or, if nothing is passed, remove the previously applied link
-   */
+  setCanonical(link: string): void {
+    this.canonicalLinkElement.href = link;
+    if (!this.document.contains(this.canonicalLinkElement)) {
+      this.document.head.appendChild(this.canonicalLinkElement);
+    }
+  }
+
+  removeCanonical(): void {
+    if (this.document.contains(this.canonicalLinkElement)) {
+      this.canonicalLinkElement.remove();
+    }
+  }
+
   updateCanonicalLink(link?: string): void {
     if (link) {
-      this.canonicalLinkElement.href = link;
-      if (!this.document.contains(this.canonicalLinkElement)) {
-        this.document.head.appendChild(this.canonicalLinkElement);
-      }
+      this.setCanonical(link);
     } else {
-      if (this.document.contains(this.canonicalLinkElement)) {
-        this.canonicalLinkElement.remove();
-      }
+      this.removeCanonical();
     }
   }
 
